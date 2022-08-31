ab6f0a97cb715642f29cc9146ecd99985f756f61
Terry Haas <terry.haas@hp.com>
2022-08-31 14:31:30 -0500

    Add CRC8 code.

---
+++ Core/Inc/crc8.h
@@ -0,0 +1,20 @@
+/*
+ * crc8.h
+ *
+ *  Created on: Aug 31, 2022
+ *      Author: HaasTerr
+ */
+
+#ifndef INC_CRC8_H_
+#define INC_CRC8_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_CRC8_H_ */
